31059561da1c2e82a218b30da5c35659b8704980
Ganesh <ganesheart1@gmail.com>
2025-01-10 21:12:44 +0530

    Update Testbench code.v

--- day_070/Testbench code.v
+++ day_070/Testbench code.v
@@ -36,55 +36,55 @@ adc_value=0;
 #5 reset=0;
 //Test case 1 - Voltage Within the range
 #10 adc_value=2000;
-$display("Test case 1 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
+  $display("Test case 1 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
 
 //Test case 2 - Over-Voltage
 #10 adc_value=4000;
-$display("Test case 1 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
+  $display("Test case 2 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
 
 //Test case 3 - Low-Voltage
 #10 adc_value=500;
-$display("Test case 1 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
+  $display("Test case 3 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
 
 //Test case 4 Random values
 #10 adc_value=$random();
-$display("Test case 1 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
+  $display("Test case 4 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
 
 //Test case 5 Random values
 #10 adc_value=$random();
-$display("Test case 1 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
+  $display("Test case 5 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
 
 //Test case 6 Random values
 #10 adc_value=$random();
-$display("Test case 1 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
+  $display("Test case 6 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
 
 //Test case 7 Random values
 #10 adc_value=$random();
-$display("Test case 1 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
+  $display("Test case 7 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
 
 //Test case 8 Random values
 #10 adc_value=$random();
-$display("Test case 1 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
+  $display("Test case 8 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
 
 //Test case 9 Random values
 #10 adc_value=$random();
-$display("Test case 1 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
+  $display("Test case 9 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
 
 //Test case 10 Random values
 #10 adc_value=$random();
-$display("Test case 1 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
+  $display("Test case 10 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
 
 //Test case 11 Random values
 #10 adc_value=$random();
-$display("Test case 1 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
+  $display("Test case 11 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
 
 //Test case 12 Random values
 #10 adc_value=$random();
-$display("Test case 1 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
+  $display("Test case 12 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
 
 //Test case 13 Random values
 #10 adc_value=$random();
-$display("Test case 1 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
+  $display("Test case 13 : ADC:%d , Over-Voltage:%b , Under-Voltage:%b , Within-Range:%b",adc_value,over_voltage,under_voltage,within_range);
 
 //end of the simulation
 
